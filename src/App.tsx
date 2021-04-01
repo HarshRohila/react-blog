add21ab2dacf0115ee9e74a6f1cda6ebc25bd184
Harsh Rohila <rohilaharsh@gmail.com>
2021-04-01 18:47:18 +0530

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Editor from './components/Editor';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import React from 'react';
+import {
+  FirebaseAuthConsumer,
+  FirebaseAuthProvider,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd,
+} from '@react-firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDHaf-Qy7OYRM5___osPlRTTAJmlFTtRjs',
+  authDomain: 'react-blog-cbf68.firebaseapp.com',
+  projectId: 'react-blog-cbf68',
+});
+
+const db = firebase.firestore();
+
+db.collection('posts')
+  .add({
+    title: 'Ada',
+    content: 'Lovelace',
+  })
+  .then((docRef) => {
+    console.log('Document written with ID: ', docRef.id);
+  })
+  .catch((error) => {
+    console.error('Error adding document: ', error);
+  });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+      <div className="App">
+        <button
+          onClick={() => {
+            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(googleAuthProvider);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Sign In with Google
+        </button>
+        <button
+          onClick={() => {
+            firebase.auth().signOut();
+          }}
+        >
+          Sign Out
+        </button>
+        <FirebaseAuthConsumer>
+          {({ isSignedIn, user, providerId }) => {
+            return (
+              <pre style={{ height: 300, overflow: 'auto' }}>
+                {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+              </pre>
+            );
+          }}
+        </FirebaseAuthConsumer>
+        <div>
+          <IfFirebaseAuthed>
+            {() => {
+              return <div>You are authenticated</div>;
+            }}
+          </IfFirebaseAuthed>
+          <IfFirebaseAuthedAnd filter={({ providerId }) => providerId !== 'anonymous'}>
+            {({ providerId }) => {
+              return <div>You are authenticated with {providerId}</div>;
+            }}
+          </IfFirebaseAuthedAnd>
+        </div>
+        <Editor />
+      </div>
+    </FirebaseAuthProvider>
   );
 }
 
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDHaf-Qy7OYRM5___osPlRTTAJmlFTtRjs",
+  authDomain: "react-blog-cbf68.firebaseapp.com",
+  projectId: "react-blog-cbf68",
+  storageBucket: "react-blog-cbf68.appspot.com",
+  messagingSenderId: "305917441200",
+  appId: "1:305917441200:web:5e14959b5ed014f6783775",
+  databaseURL: '',
+};
+
+export default firebaseConfig;
